c3f79da0f54a036ea59a52c2f4de281da9e12407
Marie <marie@ha.si>
2025-03-19 22:00:12 +0100

    update version number

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 
 #define MARIEBUILD_COLORED_LOGO
 
-#define MARIEBUILD_VERSION "0.7.3 (develop)"
+#define MARIEBUILD_VERSION "0.7.4 (develop)"
 
 /* clang-format off */
 /* clang-format fucks this macro definition up really badly somehow */
